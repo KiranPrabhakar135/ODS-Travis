7689aa3b9c3325c06aff4ccbc5040a9fc144b535
kiranprabhakar135 <kiranprabhakar135@gmail.com>
2019-11-10 23:22:48 -0500

    modifed yml

--- deploy.sh
+++ deploy.sh
@@ -10,7 +10,7 @@ echo "The servers are $servers"
 IFS=', ' read -r -a array <<< "$servers"
 for server in "${array[@]}"
 do
-	sudo scp -i ../key.pem -o StrictHostKeyChecking=no -r *.jar $server:/home/Travis-ODS
+	sudo scp -i ../ods-prod.pem -o StrictHostKeyChecking=no -r *.jar $server:/home/Travis-ODS
 	ssh -n -f -o StrictHostKeyChecking=no $server 'cd /home/Travis-ODS
 	./runProdServer.sh'
     echo "Successfully deployed on $server"
